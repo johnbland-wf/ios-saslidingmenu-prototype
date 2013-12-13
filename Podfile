8026362ddfaf48d093d0695d24a32d4b9ada7d44
John C. Bland II <john.bland@webfilings.com>
2013-12-12 17:24:57 -0700

    Initial Podfile setup with SASlidingMenu

---
+++ Podfile
@@ -0,0 +1,3 @@
+platform :ios, '7.0'
+
+pod 'SASlideMenu', git: 'https://github.com/stefanoa/SASlideMenu.git'
---
+++ Pods/Pods-environment.h
@@ -0,0 +1,14 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// SASlideMenu
+#define COCOAPODS_POD_AVAILABLE_SASlideMenu
+#define COCOAPODS_VERSION_MAJOR_SASlideMenu 2
+#define COCOAPODS_VERSION_MINOR_SASlideMenu 0
+#define COCOAPODS_VERSION_PATCH_SASlideMenu 3
+
